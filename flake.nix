433c441d0b2e96b2bf3e0b115cdeee541d590115
Kerstin Humm <kerstin@erictapen.name>
2023-04-06 18:20:29 +0200

    setup base project for server written in Rust

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    # We needed latest crate2nix for now.
+    nixpkgs-crate2nix.url = "github:erictapen/nixpkgs/crate2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-crate2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
         packages = rec {
-          hello = pkgs.hello;
-          default = hello;
+          server = let
+            crates = import ./server/Cargo.nix { inherit pkgs; };
+          in crates.workspaceMembers.isre1late-server.build;
+          default = server;
         };
         apps = rec {
           hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
@@ -21,6 +25,10 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
+            cargo
+            rustc
+            sqlite
+            (import nixpkgs-crate2nix { inherit system; }).crate2nix
           ];
         };
       }

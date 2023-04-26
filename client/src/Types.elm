c2b0a44f23129ae57f2c41a6f9347e7d94cf26f4
Kerstin Humm <kerstin@erictapen.name>
2023-04-26 18:38:47 +0200

    client: also parse id of delay

--- client/src/Types.elm
+++ client/src/Types.elm
@@ -12,6 +12,8 @@ type alias TripId =
     String
 
 
+{-| TODO find a better name for this
+-}
 type alias Delay =
     { time : Posix
     , previousStation : String
@@ -21,11 +23,14 @@ type alias Delay =
     }
 
 
-decodeClientMsg : Decoder Delay
+decodeClientMsg : Decoder ( TripId, Delay )
 decodeClientMsg =
-    map5 Delay
-        (J.map ((*) 1000 >> millisToPosix) <| field "time" int)
-        (field "previous_station" string)
-        (field "next_station" string)
-        (field "percentageSegment" float)
-        (field "delay" int)
+    map2 Tuple.pair
+        (field "id" string)
+    <|
+        map5 Delay
+            (J.map ((*) 1000 >> millisToPosix) <| field "time" int)
+            (field "previous_station" string)
+            (field "next_station" string)
+            (field "percentageSegment" float)
+            (field "delay" int)

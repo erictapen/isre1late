6ea641b3566fca4690a97428141f188f3eca4218
Kerstin Humm <kerstin@erictapen.name>
2023-07-20 12:39:53 +0200

    better reuse coverage

--- client/src/Model.elm
+++ client/src/Model.elm
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Kerstin Humm <mail@erictapen.name>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+
 module Model exposing
     ( Direction(..)
     , DistanceMatrix

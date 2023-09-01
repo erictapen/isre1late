bd0817a026a53fb7bd1b395ceb49a4bfde8e7bf4
Kerstin Humm <kerstin@erictapen.name>
2023-09-01 13:59:18 +0200

    fix reuse check

--- client/src/Components/SimpleDiagram.elm
+++ client/src/Components/SimpleDiagram.elm
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Kerstin Humm <mail@erictapen.name>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+
 module Components.SimpleDiagram exposing (view)
 
 import Dict exposing (Dict)
--- client/src/Components/TimeLegend.elm
+++ client/src/Components/TimeLegend.elm
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Kerstin Humm <mail@erictapen.name>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+
 module Components.TimeLegend exposing (view)
 
 import Html as H exposing (Html, button, div, h1, p, text)

a18a26b25e8acfa42c527bc9119fb5d92c920733
Kerstin Humm <kerstin@erictapen.name>
2023-09-01 17:14:12 +0200

    server: add index for url on fetched_html

---
+++ server/migrations/2023-09-01-150208_create_index_url_fetched_json/up.sql
@@ -0,0 +1,5 @@
+-- SPDX-FileCopyrightText: 2023 2023 Kerstin Humm <mail@erictapen.name>
+--
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+CREATE INDEX fetched_json_url_index ON fetched_json (url);

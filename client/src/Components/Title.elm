10155427d9474d5214ca6023eeecf180ecc429ad
Kerstin Humm <kerstin@erictapen.name>
2023-11-22 17:33:58 +0100

    client: remove title animation for now, better spacing

--- client/src/Components/Title.elm
+++ client/src/Components/Title.elm
@@ -6,7 +6,7 @@ module Components.Title exposing (..)
 
 import Char
 import Html exposing (Html, div, h1, span, text)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, id)
 import List exposing (filterMap, map)
 import Model exposing (Mode(..), modeString, nextMode, previousMode)
 import Msg exposing (Msg)
@@ -19,43 +19,13 @@ thinsp =
 
 view : Mode -> Float -> Html Msg
 view currentMode progress =
-    let
-        -- Unfortunately a top value higher than 100% grows the document, even
-        -- though position is set to absolute. So this is a hack to never
-        -- display elements with a top value higher than this. We make elements
-        -- progressively invisible to cover up this hack.
-        maxPos =
-            0.8
-
-        top pos =
-            fromFloat (100 * (0.01 + pos)) ++ "%"
-
-        modeH1 ( maybeMode, posOffset ) =
-            let
-                pos =
-                    0 - progress + posOffset
-            in
-            case ( maybeMode, pos < maxPos ) of
-                ( Just mode, True ) ->
-                    Just <|
-                        h1
-                            [ style "top" <| top pos
-                            , style "left" "3%"
-                            , style "opacity" <| fromFloat <| 1 - (abs pos * (1 / maxPos))
-                            ]
-                            [ text "How late is "
-                            , span [ style "background" "#e2001a", style "color" "white" ]
-                                [ text <| thinsp ++ thinsp ++ "RE1" ++ thinsp ++ thinsp
-                                ]
-                            , text <| " this " ++ modeString mode ++ "?"
-                            ]
-
-                _ ->
-                    Nothing
-    in
-    div [] <|
-        filterMap modeH1
-            [ ( previousMode currentMode, -1 )
-            , ( Just currentMode, 0 )
-            , ( nextMode currentMode, 1 )
+    div [ id "title" ]
+        [ h1
+            []
+            [ text "How late is "
+            , span [ style "background" "#e2001a", style "color" "white" ]
+                [ text <| thinsp ++ thinsp ++ "RE1" ++ thinsp ++ thinsp
+                ]
+            , text <| " this " ++ modeString currentMode ++ "?"
             ]
+        ]

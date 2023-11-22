b8e43e51a8600e7c28907f25e368ed962ef408e8
Kerstin Humm <kerstin@erictapen.name>
2023-11-22 15:42:39 +0100

    client: basic text for info text

--- client/src/Components/BottomSheet.elm
+++ client/src/Components/BottomSheet.elm
@@ -4,10 +4,10 @@
 
 module Components.BottomSheet exposing (view)
 
-import Html exposing (button, div, header, text)
+import Html exposing (button, div, h1, h2, header, text)
 import Html.Attributes exposing (attribute, class, id, style)
 import Html.Events exposing (onClick)
-import Model exposing (InfoState)
+import Model exposing (InfoState, Mode(..))
 import Msg exposing (Msg(..), TouchMsgType(..))
 import String exposing (fromFloat)
 import Utils exposing (onTouch, touchCoordinates)
@@ -28,39 +28,51 @@ view infoState mode viewportHeight =
              else
                 "true"
             )
-        , onTouch
-            "touchmove"
-            (\event -> TouchMsgBottomSheet Move <| touchCoordinates event)
-        , onTouch
-            "mousemove"
-            (\event -> TouchMsgBottomSheet Move <| touchCoordinates event)
-        , onTouch
-            "touchend"
-            (\event -> TouchMsgBottomSheet End <| touchCoordinates event)
-        , onTouch
-            "mouseup"
-            (\event -> TouchMsgBottomSheet End <| touchCoordinates event)
-        , onTouch
-            "touchcancel"
-            (\event -> TouchMsgBottomSheet Cancel <| touchCoordinates event)
         ]
         [ div [ class "overlay", onClick <| SetInfoState False ] []
         , div [ class "contents", style "height" <| fromFloat contentHeightPercent ++ "vh" ]
             [ header
                 [ class "controls"
-                , onTouch
-                    "touchstart"
-                    (\event -> TouchMsgBottomSheet Start <| touchCoordinates event)
-                , onTouch
-                    "mousedown"
-                    (\event -> TouchMsgBottomSheet Start <| touchCoordinates event)
                 ]
                 [ div
                     [ class "draggable-area"
+                    , onTouch
+                        "touchstart"
+                        (\event -> TouchMsgBottomSheet Start <| touchCoordinates event)
+                    , onTouch
+                        "mousedown"
+                        (\event -> TouchMsgBottomSheet Start <| touchCoordinates event)
+                    , onTouch
+                        "touchmove"
+                        (\event -> TouchMsgBottomSheet Move <| touchCoordinates event)
+                    , onTouch
+                        "mousemove"
+                        (\event -> TouchMsgBottomSheet Move <| touchCoordinates event)
+                    , onTouch
+                        "touchend"
+                        (\event -> TouchMsgBottomSheet End <| touchCoordinates event)
+                    , onTouch
+                        "mouseup"
+                        (\event -> TouchMsgBottomSheet End <| touchCoordinates event)
+                    , onTouch
+                        "touchcancel"
+                        (\event -> TouchMsgBottomSheet Cancel <| touchCoordinates event)
                     ]
                     [ div [ class "draggable-thumb" ] []
                     ]
                 ]
-            , div [ class "body" ] [ text "Lorem ipsum" ]
+            , div [ class "body" ] <|
+                case mode of
+                    Hour ->
+                        [ h2 [] [ text "Hour" ] ]
+
+                    Day ->
+                        [ h2 [] [ text "Day" ] ]
+
+                    Week ->
+                        [ h2 [] [ text "Week" ] ]
+
+                    _ ->
+                        [ text "Lorem ipsum" ]
             ]
         ]

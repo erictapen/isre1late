7e395be4baeadb04f7ad07a934e6520682817de2
Kerstin Humm <kerstin@erictapen.name>
2023-12-18 15:05:02 +0100

    fix reuse check

--- client/src/Components/InfoButton.elm
+++ client/src/Components/InfoButton.elm
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Kerstin Humm <mail@erictapen.name>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+
 module Components.InfoButton exposing (view)
 
 import Html exposing (button, text)
